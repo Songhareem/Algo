24b8967ccfba6e269599d1e271140541998a8a5b
Songhareem <elaha00@naver.com>
2021-04-21 10:41:17 +0900

    update

--- programmers/algo/js/level_1/핸드폰번호가리기.js
+++ programmers/algo/js/level_1/핸드폰번호가리기.js
@@ -11,7 +11,7 @@
 
 function solution(phone_number) {
 
-    return "*".repeat(phone_number.length-4) + phone_number.slice(phone_number.length-4, phone_number.length);
+    //return "*".repeat(phone_number.length-4) + phone_number.slice(phone_number.length-4, phone_number.length);
     
     return "*".repeat(phone_number.length-4) + phone_number.slice(-4);
 }
